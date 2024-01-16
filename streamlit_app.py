aeb784dc74a8b53bc95d1c33fc1ba8ec6af6c9f9
RagnarLothbrok08 <jym.caniedo@gmail.com>
2024-01-16 23:04:32 +0800

    Update streamlit_app.py
    added selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,11 @@ my_data_rows = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 streamlit.header("The fruit list contains")
 streamlit.dataframe(my_data_rows)
+
+
+streamlit.text("What fruit would you like to add?")
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),my_data_rows)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
